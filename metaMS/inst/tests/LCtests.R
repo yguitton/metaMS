08fa2001162c149139b537abdac43deca469f81e
pietrofranceschi <pietro.franceschi@fmach.it>
2014-02-21 11:55:22 +0100

    Correct Bugs in DB creation and add LC tests
    Former-commit-id: 7bcf32f76d82991c9f90897b9effa9220d675747 [formerly e3724f27204576f950f88b5bfa23d271278125cc]
    Former-commit-id: 14cf86782a77367e7c36913d43065cbd60baf617

---
+++ metaMS/inst/tests/LCtests.R
@@ -0,0 +1,51 @@
+## Script to test the main mstaMS finctions
+if (require("testthat", quietly = TRUE) & require("metaMSdata", quietly = TRUE)){
+  ## We plan to test runLC and createSTDdbLC
+  ## in both cases the function are run on the metaMSdata samples
+  ## and the outputs are compared with the ones present in the data folder
+  
+  ## <----------------   runLC -------------------------- >
+    ## run the peak picking
+    library(metaMS)
+    data(FEMsettings)  ## The settings
+    data(LCDBtest)     ## The database
+    library(metaMSdata)
+    cdfpath <- system.file("CDF_LC", package = "metaMSdata")
+    files <- list.files(cdfpath, "CDF", full.names=TRUE)
+    LC <- runLC(files, settings = Synapt.RP, DB = LCDBtest$DB)
+    
+    ## load the reference output
+    data(LCresults)
+    
+    ## Run some tests on the output
+    test_that("xcms and CAMERA are gicing consistent results", {
+      expect_equal(nrow(LC$PeakTable), nrow(LCresults$PeakTable))
+      expect_equal(ncol(LC$PeakTable), ncol(LCresults$PeakTable))
+    })
+    
+    ## Annotation gives the same results...
+    test_that("Peak table annotation gives consistent results", {
+      expect_equal(LC$Annotation$ann.features,LCresults$Annotation$ann.features)
+    })
+    
+    ## <----------------   createSTDdbLC -------------------------- >
+    data(exptable)       ## the manually curated reference table
+    exptable$stdFile <- sapply(exptable$stdFile,
+                               function(x)
+                                 files[grep(x,files)])
+    metaSetting(Synapt.RP, "DBconstruction.minfeat")  <- 2
+    
+    LCDB <- createSTDdbLC(stdInfo=exptable, 
+                          settings = Synapt.RP,
+                          polarity = "positive",
+                          Ithr = 20)
+    
+    ## Run some tests on the output
+    test_that("DB creation works fine",{
+      expect_equal(nrow(LCDB$DB), nrow(LCDBtest$DB))
+    })
+    
+    
+} else{
+  warning("cannot run tests --  package testthat and/or package metaMSdata are not available")
+}
