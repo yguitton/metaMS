59208ac1b62d2ccdf5e277b467097fc060a968b1
Ron Wehrens <ron.wehrens@gmail.com>
2014-02-21 14:25:15 +0100

    added nSlaves argument to DB creation functions
    Former-commit-id: edbb81aae45ce53b9a1be60a7d76d32ef13bd4a3 [formerly 5832575a6116734a9c39bc973f73776902f38c5b]
    Former-commit-id: 56b8a3bc0d8556b987afafcf0b236d695b7b503d

--- metaMS/R/createSTDdbLC.R
+++ metaMS/R/createSTDdbLC.R
@@ -3,7 +3,8 @@
 createSTDdbLC <- function(stdInfo,
                           settings,
                           polarity,
-                          Ithr = 10){
+                          Ithr = 10,
+                          nSlaves = 0){
   ## Read the reference table produced by the experimentalists
   
   printString("Processing the manually curated reference table")
@@ -11,15 +12,14 @@ createSTDdbLC <- function(stdInfo,
 
   
   ## Perform the peakpicking and the CAMERA analysis
-  stdxsets<- processStandards(stdInfo,settings,polarity)
+  stdxsets<- processStandards(stdInfo, settings, polarity, nSlaves = nSlaves)
   
   ## match with the reference table
-  db <- generateStdDBLC(stdxsets, metaSetting(settings, "DBconstruction"),Ithr)
+  db <- generateStdDBLC(stdxsets, metaSetting(settings, "DBconstruction"), Ithr)
   
   ## prepare an output object with some infos
   printString("Done !")
   list("Reftable" = stdInfo,
-       "Info" = list("Modified" = date(),
-                     "settings" = settings),
+       "Info" = list("Modified" = date(), "settings" = settings),
        "DB" = db)
 }
