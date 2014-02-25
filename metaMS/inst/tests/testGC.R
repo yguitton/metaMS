16cfd59ec5c3f71203e936cc7f8ee395e58eddff
Ron Wehrens <ron.wehrens@gmail.com>
2014-02-25 13:36:20 +0100

    new version
    Former-commit-id: e5632f4f2d36ac925039a3c90621f399f98ea23f

--- metaMS/inst/tests/testGC.R
+++ metaMS/inst/tests/testGC.R
@@ -1,20 +1,21 @@
+library(metaMS)
+data(FEMsettings)
+data(threeStdsNIST)
+data(threeStdsInfo)
+data(threeStdsDB)
+data(GCresults)
+
 ## Script to test the main metaMS functions for GC
 if (require("metaMSdata", quietly = TRUE)){
-  library(metaMS)
-  data(FEMsettings)
-  
   ## <----------------   createSTDdbGC -------------------------- >
   
   ## create the DB
-  data(threeStdsNIST)
-  data(threeStdsInfo)
   stdInfo[,"stdFile"] <-
       rep(list.files(system.file("CDF_GC", package = "metaMSdata"),
                      full.names = TRUE)[3], 3)
   
   DB.live <- createSTDdbGC(stdInfo, TSQXLS.GC, extDB = smallDB, nSlaves = 2)
   ## load the pre-compiled version
-  data(threeStdsDB)
   
   ## Run some tests on the output
   test_that("DB creation: equal number of DB elements",{
@@ -23,31 +24,28 @@ if (require("metaMSdata", quietly = TRUE)){
   test_that("DB creation: equal number of peaks in all DB elements",{
     expect_equal(sapply(DB, length), sapply(DB.live, length))
   })
-  
-  ## <----------------   runGC -------------------------- >
+} else {
+  message("Not testing building the data base of standards:\n\tpackage metaMSdata is necessary for that...")
 
-  ## do peak picking and annotation
-  cdfdir <- system.file("CDF_GC", package = "metaMSdata")
-  cdffiles <- list.files(cdfdir, pattern = "cdf",
-                         full.names = TRUE, ignore.case = TRUE)
-  
-  result.live <- runGC(files = cdffiles, settings = TSQXLS.GC,
-                       DB = DB, nSlaves = 2)
 
-  ## load the precompiled version, result.annot
-  data(GCresults)
-  
-  ## Run some tests on the output
-  test_that("runGC: dimensions of peak table consistent with precompiled results.", {
-    expect_equal(nrow(result.annot$PeakTable), nrow(result.live$PeakTable))
-    expect_equal(ncol(result.annot$PeakTable), ncol(result.live$PeakTable))
-  })
-    
-  ## Annotation gives the same results...
-  test_that("Annotation consistent with precompiled results.", {
-    expect_equal(result.annot$PeakTable[,"Name"],
-                 result.live$PeakTable[,"Name"])
-  })
-} else{
-  warning("cannot run tests --  package metaMSdata not available")
-}
+## <----------------   runGC -------------------------- >
+
+## Peak picking in xcms returns different numbers of features in
+## different versions. We are not interested in
+## testing this behaviour, but want to test our own code. So we
+## present the xcmsSet object rather than the file names.
+result.live <- runGC(xset = GCset, settings = TSQXLS.GC,
+                     DB = DB, nSlaves = 2)
+
+
+## Run some tests on the output
+test_that("runGC: dimensions of peak table consistent with precompiled results.", {
+  expect_equal(nrow(result.annot$PeakTable), nrow(result.live$PeakTable))
+  expect_equal(ncol(result.annot$PeakTable), ncol(result.live$PeakTable))
+})
+
+## Annotation gives the same results...
+test_that("Annotation consistent with precompiled results.", {
+  expect_equal(result.annot$PeakTable[,"Name"],
+               result.live$PeakTable[,"Name"])
+})
