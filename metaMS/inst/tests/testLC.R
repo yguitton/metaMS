5d4a23879a87daf7f90ff284a343a0e38d6e9edb
Ron Wehrens <ron.wehrens@gmail.com>
2014-02-25 13:36:20 +0100

    new version
    Former-commit-id: 672aff220d248dd95db28799551d878057e27ecd [formerly e5632f4f2d36ac925039a3c90621f399f98ea23f]
    Former-commit-id: 16cfd59ec5c3f71203e936cc7f8ee395e58eddff

--- metaMS/inst/tests/testLC.R
+++ metaMS/inst/tests/testLC.R
@@ -1,34 +1,12 @@
+library(metaMS)
+data(FEMsettings)  ## The settings
+data(LCDBtest)     ## The database
+data(LCresults)    ## The reference output
+
 ## Script to test the main metaMS functions for LC
 if (require("metaMSdata", quietly = TRUE)) {
-  ## We plan to test runLC and createSTDdbLC
-  ## in both cases the function are run on the metaMSdata samples
-  ## and the outputs are compared with the ones present in the data folder
-  
-  ## <----------------   runLC -------------------------- >
-  ## run the peak picking
-  library(metaMS)
-  data(FEMsettings)  ## The settings
-  data(LCDBtest)     ## The database
-  library(metaMSdata)
-  cdfpath <- system.file("CDF_LC", package = "metaMSdata")
-  files <- list.files(cdfpath, "CDF", full.names=TRUE)
-  LC <- runLC(files, settings = Synapt.RP, DB = LCDBtest$DB, nSlaves = 2)
-  
-  ## load the reference output
-  data(LCresults)
-  
-  ## Run some tests on the output
-  test_that("xcms and CAMERA are gicing consistent results", {
-    expect_equal(nrow(LC$PeakTable), nrow(LCresults$PeakTable))
-    expect_equal(ncol(LC$PeakTable), ncol(LCresults$PeakTable))
-  })
-  
-  ## Annotation gives the same results...
-  test_that("Peak table annotation gives consistent results", {
-    expect_equal(LC$Annotation$ann.features,LCresults$Annotation$ann.features)
-  })
-  
   ## <----------------   createSTDdbLC -------------------------- >
+
   data(exptable)       ## the manually curated reference table
   exptable$stdFile <- sapply(exptable$stdFile,
                              function(x)
@@ -43,9 +21,28 @@ if (require("metaMSdata", quietly = TRUE)) {
   ## Run some tests on the output
   test_that("DB creation works fine",{
     expect_equal(nrow(LCDB$DB), nrow(LCDBtest$DB))
-  })
-  
-  
+  })  
 } else{
-  warning("package metaMSdata not available")
+  message("Not testing building the data base of standards:\n\tpackage metaMSdata is necessary for that...")
 }
+
+## <----------------   runLC -------------------------- >
+
+## Peak picking in xcms returns different numbers of features in
+## different versions. We are not interested in
+## testing this behaviour, but want to test our own code. So we
+## present the xcmsSet object rather than the file names.
+LC <- runLC(xset = LCxset, settings = Synapt.RP, DB = LCDBtest$DB, nSlaves = 2)
+
+
+## Run some tests on the output
+test_that("xcms and CAMERA are gicing consistent results", {
+  expect_equal(nrow(LC$PeakTable), nrow(LCresults$PeakTable))
+  expect_equal(ncol(LC$PeakTable), ncol(LCresults$PeakTable))
+})
+
+## Annotation gives the same results...
+test_that("Peak table annotation gives consistent results", {
+  expect_equal(LC$Annotation$ann.features,LCresults$Annotation$ann.features)
+})
+  
