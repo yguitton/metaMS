d5e1bf5ba1d7fb3461f118f54c5c808f1cbc5321
Ron Wehrens <ron.wehrens@gmail.com>
2014-03-03 15:06:32 +0100

    using Runit tests, and now passing R CMD make check! Version 0.99.3
    Former-commit-id: 81370b42668ec272180b1ddf31ea12b6657d53c6

--- metaMS/inst/unitTests/test_GC.R
+++ metaMS/inst/unitTests/test_GC.R
@@ -5,47 +5,39 @@ data(threeStdsInfo)
 data(threeStdsDB)
 data(GCresults)
 
-## Script to test the main metaMS functions for GC
-if (require("metaMSdata", quietly = TRUE)){
-  ## <----------------   createSTDdbGC -------------------------- >
-  
-  ## create the DB
-  stdInfo[,"stdFile"] <-
-      rep(list.files(system.file("CDF_GC", package = "metaMSdata"),
-                     full.names = TRUE)[3], 3)
-  
-  DB.live <- createSTDdbGC(stdInfo, TSQXLS.GC, extDB = smallDB, nSlaves = 2)
-  ## load the pre-compiled version
-  
-  ## Run some tests on the output
-  test_that("DB creation: equal number of DB elements",{
-    expect_equal(length(DB), length(DB.live))
-  })
-  test_that("DB creation: equal number of peaks in all DB elements",{
-    expect_equal(sapply(DB, length), sapply(DB.live, length))
-  })
-} else {
-  message("Not testing building the data base of standards:\n\tpackage metaMSdata is necessary for that...")
+test_GCDBcreation <- function() {
+  ## Script to test the main metaMS functions for GC
+  if (require("metaMSdata", quietly = TRUE)){
+    ## <----------------   createSTDdbGC -------------------------- >
+    
+    ## create the DB
+    stdInfo[,"stdFile"] <-
+        rep(list.files(system.file("CDF_GC", package = "metaMSdata"),
+                       full.names = TRUE)[3], 3)
+    
+    DB.live <- createSTDdbGC(stdInfo, TSQXLS.GC, extDB = smallDB, nSlaves = 2)
+    ## load the pre-compiled version
+    
+    ## Run some tests on the output
+    checkEquals(length(DB), length(DB.live))
+    checkEquals(sapply(DB, length), sapply(DB.live, length))
+  } else {
+    message("Not testing building the data base of standards:\n\tpackage metaMSdata is necessary for that...")
+  }
 }
 
+
 ## <----------------   runGC -------------------------- >
-
-## Peak picking in xcms returns different numbers of features in
-## different versions. We are not interested in
-## testing this behaviour, but want to test our own code. So we
-## present the xcmsSet object rather than the file names.
-result.live <- runGC(xset = GCset, settings = TSQXLS.GC,
-                     DB = DB, nSlaves = 2)
-
-
-## Run some tests on the output
-test_that("runGC: dimensions of peak table consistent with precompiled results.", {
-  expect_equal(nrow(result.annot$PeakTable), nrow(result.live$PeakTable))
-  expect_equal(ncol(result.annot$PeakTable), ncol(result.live$PeakTable))
-})
-
-## Annotation gives the same results...
-test_that("Annotation consistent with precompiled results.", {
-  expect_equal(result.annot$PeakTable[,"Name"],
-               result.live$PeakTable[,"Name"])
-})
+test_GC <- function() {
+  ## Peak picking in xcms returns different numbers of features in
+  ## different versions. We are not interested in
+  ## testing this behaviour, but want to test our own code. So we
+  ## present the xcmsSet object rather than the file names.
+  result.live <- runGC(xset = GCset, settings = TSQXLS.GC,
+                       DB = DB, nSlaves = 2)
+  
+  checkEquals(nrow(result.annot$PeakTable), nrow(result.live$PeakTable))
+  checkEquals(ncol(result.annot$PeakTable), ncol(result.live$PeakTable))
+  checkEquals(result.annot$PeakTable[,"Name"], result.live$PeakTable[,"Name"])
+}
+          
--- metaMS/inst/unitTests/test_LC.R
+++ metaMS/inst/unitTests/test_LC.R
@@ -1,26 +1,21 @@
-library(metaMS)
-data(FEMsettings)  ## The settings
-data(LCDBtest)     ## The database
-data(LCresults)    ## The reference output
+test_LC <- function() {
+  ## library(metaMS)
+  data(FEMsettings)  ## The settings
+  data(LCDBtest)     ## The database
+  data(LCresults)    ## The reference output
+  
+  
+  ## <----------------   runLC -------------------------- >
+  
+  ## Peak picking in xcms returns different numbers of features in
+  ## different versions. We are not interested in
+  ## testing this behaviour, but want to test our own code. So we
+  ## present the xcmsSet object rather than the file names.
+  LC <- runLC(xset = LCxset, settings = Synapt.RP,
+              DB = LCDBtest$DB, nSlaves = 2)
 
-
-## <----------------   runLC -------------------------- >
-
-## Peak picking in xcms returns different numbers of features in
-## different versions. We are not interested in
-## testing this behaviour, but want to test our own code. So we
-## present the xcmsSet object rather than the file names.
-LC <- runLC(xset = LCxset, settings = Synapt.RP, DB = LCDBtest$DB, nSlaves = 2)
-
-
-## Run some tests on the output
-test_that("xcms and CAMERA are gicing consistent results", {
-  expect_equal(nrow(LC$PeakTable), nrow(LCresults$PeakTable))
-  expect_equal(ncol(LC$PeakTable), ncol(LCresults$PeakTable))
-})
-
-## Annotation gives the same results...
-test_that("Peak table annotation gives consistent results", {
-  expect_equal(LC$Annotation$ann.features,LCresults$Annotation$ann.features)
-})
+  checkEquals(nrow(LC$PeakTable), nrow(LCresults$PeakTable))
+  checkEquals(ncol(LC$PeakTable), ncol(LCresults$PeakTable))
+  checkEquals(LC$Annotation$ann.features,LCresults$Annotation$ann.features)
+}
   
