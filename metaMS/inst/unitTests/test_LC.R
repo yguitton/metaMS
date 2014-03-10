84fbb7ee278192a207a0c58bd4de75e5abe3fbdb
Ron Wehrens <ron.wehrens@gmail.com>
2014-03-10 12:02:22 +0100

    version 0.99.4 passing CMD check!

--- metaMS/inst/unitTests/test_LC.R
+++ metaMS/inst/unitTests/test_LC.R
@@ -7,11 +7,11 @@ test_LC <- function() {
   ## different versions. We are not interested in
   ## testing this behaviour, but want to test our own code. So we
   ## present the xcmsSet object rather than the file names.
-  LC <- runLC(xset = LCxset, settings = Synapt.RP,
+  LC <- runLC(xset = LCresults$xset, settings = Synapt.RP,
               DB = LCDBtest$DB, nSlaves = 2)
 
   checkEquals(nrow(LC$PeakTable), nrow(LCresults$PeakTable))
   checkEquals(ncol(LC$PeakTable), ncol(LCresults$PeakTable))
-  checkEquals(LC$Annotation$ann.features,LCresults$Annotation$ann.features)
+  checkEquals(LC$Annotation$ann.features, LCresults$Annotation$ann.features)
 }
   
