81a287fb4bbf01978b797ed6ddf592b7779fd5e8
Ron Wehrens <ron.wehrens@gmail.com>
2014-03-10 10:43:43 +0100

    first version of 0.99.4 and 0.99.1 (metaMSdata)

--- metaMS/inst/unitTests/test_GC.R
+++ metaMS/inst/unitTests/test_GC.R
@@ -1,43 +1,17 @@
-library(metaMS)
-data(FEMsettings)
-data(threeStdsNIST)
-data(threeStdsInfo)
-data(threeStdsDB)
-data(GCresults)
-
-test_GCDBcreation <- function() {
-  ## Script to test the main metaMS functions for GC
-  if (require("metaMSdata", quietly = TRUE)){
-    ## <----------------   createSTDdbGC -------------------------- >
-    
-    ## create the DB
-    stdInfo[,"stdFile"] <-
-        rep(list.files(system.file("CDF_GC", package = "metaMSdata"),
-                       full.names = TRUE)[3], 3)
-    
-    DB.live <- createSTDdbGC(stdInfo, TSQXLS.GC, extDB = smallDB, nSlaves = 2)
-    ## load the pre-compiled version
-    
-    ## Run some tests on the output
-    checkEquals(length(DB), length(DB.live))
-    checkEquals(sapply(DB, length), sapply(DB.live, length))
-  } else {
-    message("Not testing building the data base of standards:\n\tpackage metaMSdata is necessary for that...")
-  }
-}
-
-
-## <----------------   runGC -------------------------- >
 test_GC <- function() {
+  data(FEMsettings)   ## settings
+  data(threeStdsDB)   ## annotation database
+  data(GCresults)     ## pre-computed results
+  
   ## Peak picking in xcms returns different numbers of features in
   ## different versions. We are not interested in
   ## testing this behaviour, but want to test our own code. So we
   ## present the xcmsSet object rather than the file names.
-  result.live <- runGC(xset = GCset, settings = TSQXLS.GC,
+  result.live <- runGC(xset = GCresults$xset, settings = TSQXLS.GC,
                        DB = DB, nSlaves = 2)
   
-  checkEquals(nrow(result.annot$PeakTable), nrow(result.live$PeakTable))
-  checkEquals(ncol(result.annot$PeakTable), ncol(result.live$PeakTable))
-  checkEquals(result.annot$PeakTable[,"Name"], result.live$PeakTable[,"Name"])
+  checkEquals(nrow(GCresults$PeakTable), nrow(result.live$PeakTable))
+  checkEquals(ncol(GCresults$PeakTable), ncol(result.live$PeakTable))
+  checkEquals(GCresults$PeakTable[,"Name"], result.live$PeakTable[,"Name"])
 }
           
