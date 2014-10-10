04217022b01060012731ba6cb2ec9bf61a52a437
r.wehrens <r.wehrens@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-10-10 11:50:42 +0000

    Commit made by the Bioconductor Git-SVN bridge. Consists of 1 commit.
    Commit information:
    
    Commit id: 1f6a2824e5a564774c37b949507018d985dbb5dc
    
        Modify alignmentLC to work with Obiwarp Added settings for Orbitrap
    
    Committed by: Pietro Franceschi
    Author Name: Pietro Franceschi
    Commit date: 2014-10-10 13:49:31 +0200
    Author date: 2014-10-10 13:49:31 +0200
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/metaMS@95288 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/alignmentLC.R
+++ R/alignmentLC.R
@@ -1,50 +1,66 @@
 alignmentLC <- function (xset, settings){
-  ## set the extra and missing on the bases of the 
-  ## number of samples
-  missing <- ceiling((settings$missingratio) * length(xset@filepaths))
-  extra <- ceiling((settings$extraratio) * length(xset@filepaths))
-  
+  ## if only one sample do not retcor ....
   if (length(sampnames(xset)) == 1){
-    return(xset)
-  } else{
-    myminsamp  <- min(c(settings$min.class.size, 
-                        ceiling(length(sampnames(xset))*settings$min.class.fraction)))
-    printString("minsamp:",myminsamp)
-    
-    ## --------  xcms grouping ----------------------------
-    xset <- do.call(group,
-                    c(list(object = xset, 
-                           bw = settings$bws[1],
-                           minsamp = myminsamp,
-                           minfrac = 0),
-                      settings["mzwid"]))
-    
-    ## ------- Retention time correction  ------------------
-    printString("missing:", missing)
-    printString("extra:", extra)
-    xset <- retcor(xset,  
-                   method  = settings$retcormethod,
-                   family = settings$retcorfamily,
-                   missing = missing,
-                   extra = extra) 
-    
-    
-    
-    xset <- do.call(group,
-                    c(list(object = xset, 
-                           bw = settings$bws[2],
-                           minsamp = myminsamp,
-                           minfrac = 0),
-                      settings["mzwid"]))
-    
-    
+      return(xset)
+    } else {
+      ## calculate min frac 
+      myminsamp  <- min(c(settings$min.class.size, 
+        ceiling(length(sampnames(xset))*settings$min.class.fraction)))
+      printString("minsamp:",myminsamp)
+
+      ## check if "method" is among the settings ...
+       if ("method" %in% names(settings)){
+        ## check if it is obiwarp ...
+        if (settings$method == "obiwarp") {
+        ## Do it with obiwarp ....
+         printString("obiwarp retcor")
+         xset <- retcor(xset,  
+          method  = settings$method,
+          profStep = settings$profStep)
+         ## Grouping 
+         xset <- do.call(group,
+          c(list(object = xset, 
+            bw = settings$bw,
+            minsamp = myminsamp,
+            minfrac = 0),
+          settings["mzwid"]))
+      }
+    } else {
+        ## use the default density based approach
+        ## set the extra and missing on the bases of the 
+        ## number of samples
+        missing <- ceiling((settings$missingratio) * length(xset@filepaths))
+        extra <- ceiling((settings$extraratio) * length(xset@filepaths))
+        ## --------  xcms grouping ----------------------------
+        xset <- do.call(group,
+          c(list(object = xset, 
+            bw = settings$bws[1],
+            minsamp = myminsamp,
+            minfrac = 0),
+          settings["mzwid"]))
+         ## ------- Retention time correction  ------------------
+        printString("missing:", missing)
+        printString("extra:", extra)
+        xset <- retcor(xset,  
+          method  = settings$retcormethod,
+          family = settings$retcorfamily,
+          missing = missing,
+          extra = extra) 
+        ## --------- Second Grouping  
+        xset <- do.call(group,
+          c(list(object = xset, 
+            bw = settings$bws[2],
+            minsamp = myminsamp,
+            minfrac = 0),
+          settings["mzwid"]))
+   }
     if (settings$fillPeaks) {
       printString("Filling missing peaks")
       fillPeaks(xset)
     } else {
       xset
     }
-  }
-  
+  }  
 }
+
 
