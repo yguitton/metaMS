985f13db3785710e4fef54f4dad3a1502d18199a
Ron Wehrens <ron.wehrens@gmail.com>
2015-01-15 16:26:28 +0000

    Commit made by the Bioconductor Git-SVN bridge. Consists of 1 commit.
    Commit information:
    
    Commit id: b51bd420247ea128abe2becdb717db63818a4e28 [formerly 119442a07bc6166b70c7f5c0178f34e9b92931ac] [formerly 8617f59df39ca3fa9585fbb129c785c4d7ce56af [formerly 849f967a6d8b2089c4e6fd7473ee9565b05b7da1]]
    
        Version 1.3.2
    
    Committed by: pietrofranceschi
    Author Name: pietrofranceschi
    Commit date: 2015-01-15 17:25:39 +0100
    Author date: 2015-01-15 17:25:39 +0100
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/metaMS@98365 bc3139a8-67e5-0310-9ffc-ced21a209358
    
    Former-commit-id: 9155014f18b6ca202da12ed7a56bd7500f409839 [formerly 2964131125daa0cd9801ed89e6dd7230e2c2b4c1]
    Former-commit-id: 36d009c141c30d662389ec8410ae52834e6dccef

--- R/getAnnotationLC.R
+++ R/getAnnotationLC.R
@@ -5,53 +5,38 @@ getAnnotationLC <- function(xs,
                             errf)
 {
   
-  # ------- Peak table with maxo---------------------------------
-  
-  
+  # ------- Peak table with maxo  --------------------------------- > 
   
   PkT4ann <- getPeakTable(xs, intval = "maxo")
-  rt.id <- which(colnames(PkT4ann) == "rt")
-  DM <- PkT4ann[,((rt.id+1):dim(PkT4ann)[2]), drop = FALSE]
-  
-  ## check the class of the object to set the the 
-  if (class(xs)[1] == "xcmsSet") {
-    nsamp  <- length(xs@filepaths)
+  dataMatrix <- PkT4ann[,rownames(xs@xcmsSet@phenoData),drop = FALSE]
+
+  ## check the number of samples
+  if ( length(xs@xcmsSet@filepaths) == 1) {
+    I <- dataMatrix
   } else {
-    nsamp  <- length(xs@xcmsSet@filepaths)
+    I <- apply(dataMatrix,1,median) ## use the median intensity in DM for annotation
   }
-  
-  if (nsamp == 1) {
-    I <- DM
-  } else {
-    I <- apply(DM,1,max) ## Change this
-  }
-  
-  
-  
-  ## -------- Table to be annotate
+
+  ## -------- Table to be annotate --------------------------------- > 
   tobeannotated <- cbind("mz" = PkT4ann$mz, "rt" = PkT4ann$rt, "I" = unlist(I))
   
-
-  
+  ## complete annotation information 
   out.raw <- AnnotateTable(tobeannotated,
                            errf = errf,
                            DB = DB,
                            settings = settings)
   
+  ## subset of the annotation output to be used in the metaMS output
   ann.ID <- rep("", dim(tobeannotated)[1])
   ann.compound <- rep("", dim(tobeannotated)[1])
-  
-  
   annotated.features <- unique(out.raw$annotation.table$feature)
-  
-  
   for (id in annotated.features){
     ann.ID[id] <- paste(out.raw$annotation.table$ChemSpiderID[out.raw$annotation.table$feature == id], collapse = " ")
     ann.compound[id] <- paste(out.raw$annotation.table$compound[out.raw$annotation.table$feature == id], collapse = " ")
   } 
   
   
-  ## ------ Format the output 
+  ## ------ Format the output -------------------------------------- >
   
   list(raw = out.raw,
        for_table = data.frame(ChemSpiderID = ann.ID,
