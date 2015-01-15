36d009c141c30d662389ec8410ae52834e6dccef
Ron Wehrens <ron.wehrens@gmail.com>
2015-01-15 16:26:28 +0000

    Commit made by the Bioconductor Git-SVN bridge. Consists of 1 commit.
    Commit information:
    
    Commit id: 119442a07bc6166b70c7f5c0178f34e9b92931ac [formerly 849f967a6d8b2089c4e6fd7473ee9565b05b7da1]
    
        Version 1.3.2
    
    Committed by: pietrofranceschi
    Author Name: pietrofranceschi
    Commit date: 2015-01-15 17:25:39 +0100
    Author date: 2015-01-15 17:25:39 +0100
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/metaMS@98365 bc3139a8-67e5-0310-9ffc-ced21a209358
    
    Former-commit-id: 2964131125daa0cd9801ed89e6dd7230e2c2b4c1

--- R/getPeakTable.R
+++ R/getPeakTable.R
@@ -2,68 +2,57 @@ getPeakTable <- function (xs, intval = "into")
 {
   ## get peak table from XCMS object  ------------------------------------------
   if (class(xs)[1] == "xcmsSet") { 
-    if (length(xs@filepaths) == 1){
-      
-      sortorder <- order(xs@peaks[,"rt"])
-      
+    if (length(xs@filepaths) == 1){     
+      sortorder <- order(xs@peaks[,"rt"])   ## order by retention time  
       peakTable <- data.frame("mz" = xs@peaks[sortorder, "mz"],
                               "rt" = xs@peaks[sortorder, "rt"]/60,
                               "intensity" = xs@peaks[sortorder,intval],
                               row.names = NULL,
                               stringsAsFactors = FALSE)
+      ## put the right title to the intensity column
       colnames(peakTable)[colnames(peakTable) == "intensity"] <- intval
-      
-      peakTable
-    } else {
-      
-      sortorder <- order(xs@groups[,"rtmed"])
-      dataMatrix <- groupval(xs, value=intval)
-      
-      peakTable <- data.frame(dataMatrix[sortorder,], row.names=NULL)
-      data.frame("mz" = xs@groups[sortorder, "mzmed"],
-                 "rt" = xs@groups[sortorder, "rtmed"]/60,
-                 dataMatrix[sortorder,],
-                 row.names = NULL,
-                 stringsAsFactors = FALSE) 
-    }    
-  } else {
-    ## get peak table from CAMERA object  ------------------------------
-    ##  if (class(xs)[1] == "xsAnnotate") {     
-    pt  <-  getPeaklist(xs, intval=intval)  # function of CAMERA package
-    ## sort the table by rt
-    sortid <- order(pt$rt)
-    pt <- pt[sortid,]
+      return(peakTable)
+    }
+    ## if Xset object and more than one sample
+    sortorder <- order(xs@groups[,"rtmed"])      ## rt ordering
+    dataMatrix <- groupval(xs, value=intval)     ## extract the intensities
+    peakTable <- data.frame("mz" = xs@groups[sortorder, "mzmed"],
+                            "rt" = xs@groups[sortorder, "rtmed"]/60,
+                            dataMatrix[sortorder,],
+                            row.names = NULL,
+                            stringsAsFactors = FALSE)
+    return(peakTable)
+
+  } else {   ## get peak table from CAMERA object  ------------------------------    
+    pt  <-  getPeaklist(xs, intval=intval)  ## function of CAMERA package
+    sortorder <- order(pt[,"rt"])           ## rt ordering
+    pt <- pt[sortorder,]
+    ## also here one sample ....
     if (length(xs@xcmsSet@filepaths) == 1){
-      
-      I  <- pt[, intval]
-      
       peakTable <- data.frame("pcgroup" = as.numeric(pt$pcgroup),
                               "adduct" = pt$adduct,
                               "isotopes" = pt$isotopes,
                               "mz" = pt$mz,
                               "rt" = pt$rt/60,
-                              "intensity" = I,
+                              "intensity" = pt[, intval],
                               row.names=NULL,
                               stringsAsFactors = FALSE)
+      ## put the right title to the intensity column
       colnames(peakTable)[colnames(peakTable) == "intensity"] <- intval
-      
-      peakTable
-    } else {
-      
-      ## organize the output
-      DM <- pt[,9:(dim(pt)[2]-3)]
-      peakTable <- data.frame(DM, row.names=NULL)
-      data.frame("pcgroup" = as.numeric(pt$pcgroup),
-                 "adduct" = pt$adduct,
-                 "isotopes" = pt$isotopes,
-                 "mz" = pt$mz,
-                 "rt" = pt$rt/60,
-                 DM,
-                 row.names = NULL,
-                 stringsAsFactors = FALSE) 
-      
-    } 
+      return(peakTable)
+    } ## more samples
+    dataMatrix <- pt[,rownames(xs@xcmsSet@phenoData)]  ## get only the columns with the intensities
+    peakTable <- data.frame("pcgroup" = as.numeric(pt$pcgroup),
+                            "adduct" = pt$adduct,
+                            "isotopes" = pt$isotopes,
+                            "mz" = pt$mz,
+                            "rt" = pt$rt/60,
+                            dataMatrix,
+                            row.names = NULL,stringsAsFactors = FALSE)
+    return(peakTable)
   }
-} 
+}
+
+      
 
 
