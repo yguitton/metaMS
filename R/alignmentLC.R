6c78eae47cd594d821edee4a0937a1fc8fd25cf6
Ron Wehrens <ron.wehrens@gmail.com>
2015-03-20 10:26:08 +0100

    saved GCresults object also containing xset
    Former-commit-id: 15bdf93985e1920a3db790912cca97037682d79c

--- R/alignmentLC.R
+++ R/alignmentLC.R
@@ -10,7 +10,7 @@ alignmentLC <- function (xset, settings){
   
   ## The workflow is 1) grouping, 2) retcor, 3) grouping, optionally fillpeaks 
   ## for density based two grouping runs are required (before and after retcor)
-  ##  for obiwarp only on eis necessary (the first one is useless)
+  ## for obiwarp only one is necessary (the first one is useless)
       
   ## identify the two bandwidths for grouping ------------------------------- >
   if (length(settings$bws) == 2) {
@@ -21,14 +21,17 @@ alignmentLC <- function (xset, settings){
     bw2 = settings$bws[1]
   }
   ## perform the first grouping --------------------------------------------- >
-  xset <- do.call(group, c(list(object = xset, bw = bw1, minsamp = myminsamp,minfrac = 0),settings["mzwid"]))
+  xset <- do.call(group,
+                  c(list(object = xset, bw = bw1,
+                         minsamp = myminsamp, minfrac = 0),
+                    settings["mzwid"]))
   
   ## perform the retention time correction ---------------------------------- >
   ## create a list of parameters to use do.call 
   if ("obiwarp" %in% settings$Retcor) {   
     ## if obiwarp use only the provided parameters
     printString("Obiwarp retcor")
-    retcorlist <- c(list(object = xset),settings$Retcor)
+    retcorlist <- c(list(object = xset), settings$Retcor)
   } else {
     ## use the "standard" density based idea
     printString("Density-based retcor")
@@ -37,15 +40,18 @@ alignmentLC <- function (xset, settings){
     extra <- ceiling((settings$extraratio) * length(xset@filepaths))
     printString("missing:", missing)
     printString("extra:", extra)
-    retcorlist <- c(list(object = xset, missing = missing, extra = extra), settings$Retcor)
+    retcorlist <- c(list(object = xset, missing = missing, extra = extra),
+                    settings$Retcor)
   }
   # do the real retention time correction
-  xset <- do.call(retcor,retcorlist)
+  xset <- do.call(retcor, retcorlist)
 
-  # perform the second round of grouping  ------------------------------------- >
-  xset <- do.call(group,c(list(object = xset,  bw = bw2, minsamp = myminsamp, minfrac = 0), settings["mzwid"]))
+  # perform the second round of grouping ------------------------------------- >
+  xset <- do.call(group,c(list(object = xset,  bw = bw2,
+                               minsamp = myminsamp, minfrac = 0),
+                          settings["mzwid"]))
 
-  # optional fill missing peaks ----------------------------------------------- >
+  # optional fill missing peaks ---------------------------------------------- >
   if (settings$fillPeaks) {
     printString("Filling missing peaks")
     fillPeaks(xset)
