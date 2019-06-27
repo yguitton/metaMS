a32c519a8d7ef36d3816f24a85642dbc3395665f
jsaintvanne <julien.saint-vanne@sb-roscoff.fr>
2019-06-27 12:51:06 +0200

    Changes when rt.matches =0

--- R/matchSamples2DB.R
+++ R/matchSamples2DB.R
@@ -64,29 +64,29 @@ matchSamples2DB <- function(xset.msp,
     ## standard compound. Slower, obviously.
     match.results <-
         lapply(1:length(xset.msp),
-               function(ii) {
-                 result <- matrix(0, length(DB), length(xset.msp[[ii]]))
-                 for (i in 1:nrow(rt.matches[[ii]])) {
-                   DB.idx <- rt.matches[[ii]][i,1]
-                   sample.idx <- rt.matches[[ii]][i,2]
-                   exp.pat <- xset.msp[[ii]][[sample.idx]]
-
-                   MWlimit <- DB[[DB.idx]]$monoMW + 4
-                   ## sometimes, with manually added spectra, monoMW
-                   ## is not present... then we use everything up
-                   ## until the highest mass present.
-                   if (length(MWlimit) == 0)
-                       MWlimit <- max(DB[[DB.idx]]$pspectrum[,1])
+              function(ii) {
+                result <- matrix(0, length(DB), length(xset.msp[[ii]]))
+                for (i in 1:nrow(rt.matches[[ii]])) {
+                  if(nrow(rt.matches[[ii]] > 0)){
+                    DB.idx <- rt.matches[[ii]][i,1]
                    
-                   ok.mz <- which(exp.pat[,"mz"] <= MWlimit)
-                   if (length(ok.mz) > settings$minfeat) {
-                     exp.pat <- treat.DB(list(exp.pat[ok.mz,]), isMSP = FALSE)
-                     result[DB.idx, sample.idx] <-
-                         mzmatch(DB[[DB.idx]]$pspectrum, exp.pat[[1]])
-                   }
-                 }
-                 
-                 result})
+                    sample.idx <- rt.matches[[ii]][i,2]
+                    exp.pat <- xset.msp[[ii]][[sample.idx]]
+                    MWlimit <- DB[[DB.idx]]$monoMW + 4
+                    ## sometimes, with manually added spectra, monoMW
+                    ## is not present... then we use everything up
+                    ## until the highest mass present.
+                    if (length(MWlimit) == 0)
+                      MWlimit <- max(DB[[DB.idx]]$pspectrum[,1])
+                    ok.mz <- which(exp.pat[,"mz"] <= MWlimit)
+                    if (length(ok.mz) > settings$minfeat) {
+                      exp.pat <- treat.DB(list(exp.pat[ok.mz,]), isMSP = FALSE)
+                      result[DB.idx, sample.idx] <-
+                        mzmatch(DB[[DB.idx]]$pspectrum, exp.pat[[1]])
+                    }
+                  }
+                }
+                result})
   }
   names(match.results) <- names(xset.msp)
   
@@ -94,9 +94,10 @@ matchSamples2DB <- function(xset.msp,
       lapply(match.results,
              function(xx) {
                sapply(1:ncol(xx),
-                      function(ii)
-                      which(xx[,ii] > settings$simthresh))})
-  
+                      function(ii){
+                      which(xx[,ii] > settings$simthresh)
+                      })
+              } )
   list(annotations = mapply(annotations2tab,
            annotations,
            match.results, SIMPLIFY = FALSE))
