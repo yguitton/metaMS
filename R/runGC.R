fc7a6275e37e2b2bdc1de2f14df12f99ef9f0158
jsaintvanne <julien.saint-vanne@sb-roscoff.fr>
2019-08-20 16:04:26 +0200

    correct indentation
    Former-commit-id: 03ce1df9462e26d99b77ba59a4ccac9078748d9c [formerly ec8eebdc505c470e39a20f262ebe6cbbf3f38f80]
    Former-commit-id: ce45f55a6363ec8e8af7840924de1f19b6d037b2

--- R/runGC.R
+++ R/runGC.R
@@ -23,7 +23,6 @@ runGC <- function(files,
     xset.l <- xset
     nexp <- length(xset.l)
   }
-
   ## same criterion as in matchSamples2Samples
   mcs <-
       max(2,
@@ -35,6 +34,7 @@ runGC <- function(files,
   if (findUnknowns & nexp < mcs) {
     stop("Number of samples too small to define unknowns - either provide more samples or change the settings.")
   }
+
   if (is.null(DB) & !findUnknowns)
       stop("Nothing to do. Provide a DB or set 'findUnknowns' to TRUE...")
   
@@ -87,7 +87,6 @@ runGC <- function(files,
     printString("Using xcmsSet object - only doing annotation")
     allSamples <- xset.l
   }
-
   ## ###################################################################
   ## convert into msp format (a nested list)
   allSamples.msp <- lapply(allSamples,
@@ -99,7 +98,6 @@ runGC <- function(files,
   if (!is.null(RIstandards))
       allSamples.msp <- lapply(allSamples.msp, addRI,
                                RIstandards, isMSP = FALSE)
-
   ## check: files without any features - should not happen very often...
   nofeats <- which(sapply(allSamples.msp, length) == 0)
   if ((nnof <- length(nofeats)) > 0) {
@@ -202,6 +200,7 @@ runGC <- function(files,
     ann.df <- getAnnotationMat(exp.msp = allSamples.msp,
                                pspectra = PseudoSpectra,
                                allMatches = allSam.matches)
+
     ## To get to intensities comparable to the ones identified by xcms,
     ## use largest peak in PseudoSpectra as the common intensity measure
     ann.df2 <-
