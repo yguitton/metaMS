7e410831047878cd551af413439c1328e2440832
yguitton <yann.guitton@gmail.com>
2020-04-21 18:03:37 +0200

    Indentation cleanning after formatR
    Former-commit-id: d50b741aba58e2cd64b2856f64a79f29b0d4c7b9

--- R/plotPseudoSpectrum.R
+++ R/plotPseudoSpectrum.R
@@ -1,8 +1,7 @@
-## Auxiliary function for plotting a particular pseudospectrum. M/z
-## values are in the first column of the matrix, and an intensity
-## measure (either maxo, into or something else) in the second. The third
-## column is disregarded, usually contains retention time information
+## Auxiliary function for plotting a particular pseudospectrum. M/z values are in the first column of the matrix, and an
+## intensity measure (either maxo, into or something else) in the second. The third column is disregarded, usually contains
+## retention time information
 plotPseudoSpectrum <- function(psspc, ...) {
-  plot(psspc[,1:2], type = "h", col = 4, xlab = "m/z", ylab = "I", ...)
+    plot(psspc[, 1:2], type = "h", col = 4, xlab = "m/z", ylab = "I", ...)
 }
 
